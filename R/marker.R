19bee60522568f7005c3483b1a118a23949a71cd
magnusdv <magnusdv@medisin.uio.no>
2018-08-11 21:25:17 +0200

    Move mutation stuff to "marker_mutations.R"

--- R/marker.R
+++ R/marker.R
@@ -177,41 +177,6 @@ marker = function(x, ...,  alleles = NULL, afreq = NULL, chrom = NA,
   m
 }
 
-.checkMutationMatrix = function(mutmat, alleles) {
-    # Check that mutation matrix is compatible with allele number / names.  Sort matrix
-    # according to the given allele order (this is important since dimnames are NOT used in
-    # calculations).
-  alleles = as.character(alleles)
-  N = length(alleles)
-
-  if (any((dm <- dim(mutmat)) != N)) {
-    print(mutmat)
-    stop2(sprintf("Dimension of mutation matrix (%d x %d)
-                     inconsistent with number of alleles (%d)", dm[1], dm[2], N))
-  }
-
-  if (any(round(.rowSums(mutmat, N, N), 3) != 1)) {
-    print(mutmat)
-    stop2("Row sums of mutation matrix are not 1")
-  }
-
-  row_nms = rownames(mutmat)
-  col_nms = colnames(mutmat)
-  if (!setequal(row_nms, alleles) || !setequal(col_nms, alleles)) {
-    print(mutmat)
-    stop2("Dimnames of mutation do not match allele names")
-  }
-
-  m = mutmat[alleles, alleles]
-
-  # Lumbability: always lumpable (any alleles) if rows are identical (except diagonal)
-  attr(m, "lumpability") = NA
-  if (N > 2) {
-    if (all(vapply(1:N, function(i) diff(range(m[-i, i])) == 0, logical(1))))
-      attr(m, "lumpability") = "always"  # If each column has 0 range
-  }
-  m
-}
 
 .setSNPfreqs = function(x, newfreqs) { # TODO: review function
     stopifnot(all(vapply(x$markerdata, function(m) attr(m, "nalleles"), numeric(1)) == 2))
