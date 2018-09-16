b55c4dcd37e900e30eeec8aad63c490fd6f78227
magnusdv <magnusdv@medisin.uio.no>
2018-09-16 20:56:36 +0200

    Add singleton tests

--- tests/testthat/test-singleton.R
+++ tests/testthat/test-singleton.R
@@ -1,21 +1,23 @@
 context("singletons")
 
+s = singleton(1)
+
 test_that("singletons are constructed", {
-  expect_error(singleton(1), NA)
+  expect_error(s, NA)
   expect_error(singleton("A", sex=2, famid="FAM"), NA)
 })
 
 test_that("singleton modifications commute", {
-  x = relabel(swapSex(singleton(1), 1), 10)
-  y = swapSex(relabel(singleton(1), 10), 10)
+  x = relabel(swapSex(s, 1), 10)
+  y = swapSex(relabel(s, 10), 10)
   expect_identical(x,y)
 
-  z = relabel(singleton(1), old=1, new="A")
+  z = relabel(s, old=1, new="A")
   expect_identical(z, singleton("A"))
 })
 
 test_that("addChildren works on singleton", {
-  x = addChildren(singleton(1), father=1)
+  x = addChildren(s, father=1)
   expect_identical(x, nuclearPed(1))
 })
 
@@ -23,4 +25,31 @@ test_that("addParents works on singleton", {
   x = addParents(singleton(3), 3, father=1, mother=2)
   x = parents_before_children(x)
   expect_identical(x, nuclearPed(1))
+})
+
+test_that("singleton relatives are all empty", {
+  expect_identical(father(s, 1), character(0))
+  expect_identical(mother(s, 1), character(0))
+  expect_identical(parents(s, 1), character(0))
+  expect_identical(grandparents(s, 1), character(0))
+  expect_identical(spouses(s, 1), character(0))
+  expect_identical(children(s, 1), character(0))
+  expect_identical(siblings(s, 1), character(0))
+  expect_identical(cousins(s, 1), character(0))
+  expect_identical(cousins(s, 1, half = T), character(0))
+  expect_identical(cousins(s, 1, deg = 0, removal = 1), character(0))
+  expect_identical(unrelated(s, 1), character(0))
+  expect_identical(ancestors(s, 1), character(0))
+  expect_identical(descendants(s, 1), character(0))
+})
+
+test_that("singleton subgroups are as expected", {
+  expect_identical(leaves(s), "1")
+  expect_identical(founders(s), "1")
+  expect_identical(nonfounders(s), character(0))
+  expect_identical(males(s), "1")
+  expect_identical(females(s), character(0))
+  expect_identical(typedMembers(s), character(0))
+  expect_identical(untypedMembers(s), "1")
+
 })
