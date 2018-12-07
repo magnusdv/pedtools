6a70d30b7ea9e1ca57a8ea14699fbc6d0de69b4b
magnusdv <magnusdv@medisin.uio.no>
2018-12-07 15:10:50 +0100

    Faster implementation of `connectedComponents()`

--- R/ped_connected.R
+++ R/ped_connected.R
@@ -7,7 +7,8 @@
 #' @param id A vector of ID labels (character or numeric)
 #' @param fid The ID labels of the fathers (or "0" if missing)
 #' @param mid The ID labels of the mothers (or "0" if missing)
-#'
+#' @param fidx,midx (For internal use mostly) Integer vectors with paternal
+#'   (resp maternal) indices. These may be given instead of `id`, `fid`, `mid`.
 #' @return A list, where each element is a subset of `id` constituting a
 #'   connected pedigree
 #'
@@ -17,7 +18,48 @@
 #' connectedComponents(x$id, x$fid, x$mid)
 #'
 #' @export
-connectedComponents = function(id, fid, mid) {
+connectedComponents = function(id, fid, mid, fidx=NULL, midx=NULL) {
+  if(!missing(id)) {
+    fidx = match(fid, id, nomatch = 0L)
+    midx = match(mid, id, nomatch = 0L)
+  }
+  else {
+    id = seq_along(fidx)
+  }
+  seqn = seq_along(id)
+
+  adjacencyList = lapply(seqn, function(i) {
+    fa = fidx[i]; mo = midx[i]
+    c(if(fa > 0) fa, if(mo > 0 && mo != fa) mo, seqn[fidx == i | midx == i])
+  })
+
+  env = new.env()
+  env$comp = integer(length(fidx))
+
+  DFS = function(i, tag) {
+    env$comp[i] = tag
+    for(j in adjacencyList[[i]]) {
+      if(env$comp[j] == 0)
+        DFS(j, tag)
+    }
+  }
+
+  founders = seqn[fidx == 0 & midx == 0]
+  tag = 0
+  for(fou in founders) {
+    if(env$comp[fou] == 0) {
+      tag = tag + 1
+      DFS(fou, tag)
+    }
+  }
+
+  comps = env$comp
+  lapply(1:max(comps), function(i) id[comps == i])
+}
+
+
+# Old version
+connectedComponents_OLD = function(id, fid, mid) {
   # Placeholder for final components
   comps = list()
 
