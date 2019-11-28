9bbe0632b25df57512d7c70e4a1a9e4bc6309ce4
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2019-11-28 15:49:36 +0100

    Allow boolean selection in `whichMarkers()` and friends

--- R/marker_select.R
+++ R/marker_select.R
@@ -1,28 +1,30 @@
 #' Select or remove attached markers
 #'
-#' Functions for manipulating markers attached to a `ped` object.
+#' Functions for manipulating markers attached to `ped` objects.
 #'
-#' @param x A `ped` object, or (in `selectMarkers()` and `removeMarkers()`) a
-#'   list of such
+#' @param x A `ped` object, or a list of such
 #' @param markers Either a character vector (with marker names), a numeric
-#'   vector (with marker indices), or NULL
+#'   vector (with marker indices), a logical (of the same length as the number
+#'   of markers attached to `x`), or NULL
 #' @param chroms A vector of chromosome names, or NULL
 #' @param fromPos A single number or NULL
 #' @param toPos A single number or NULL
 #'
 #' @return The return values of these functions are:
 #'
-#'   * `selectMarkers()`: a `ped` object where only the indicated markers are
-#'   kept
+#'   * `selectMarkers()`: an object identical to `x`, but where only the
+#'   indicated markers are kept
 #'
-#'   * `removeMarkers()`: a `ped` object where the indicated markers are removed
+#'   * `removeMarkers()`: an object identical to `x`, but where the indicated
+#'   markers are removed
 #'
-#'   * `getMarkers()`: a list of `marker` objects
+#'   * `getMarkers()`: a list of `marker` objects. Note: If `x` is a list of
+#'   pedigrees, the marker objects attached to the first component will be
+#'   returned.
 #'
-#'   * `whichMarkers()`: an integer vector with indices of the indicated markers
-#'
-#'   In `selectMarkers()` and `removeMarkers()` the first argument may be a list
-#'   of `ped` objects, in which case the output will be the same.
+#'   * `whichMarkers()`: an integer vector with indices of the indicated
+#'   markers. If `x` is a list of pedigrees an error is raised unless
+#'   `whichMarkers()` gives the same result for all components.
 #'
 #' @seealso [`setMarkers()`]
 #'
@@ -46,6 +48,9 @@ selectMarkers = function(x, markers = NULL, chroms = NULL, fromPos = NULL, toPos
 #' @rdname marker_select
 #' @export
 getMarkers = function(x, markers = NULL, chroms = NULL, fromPos = NULL, toPos = NULL) {
+  if(is.pedList(x))
+    x = x[[1]]
+
   idx = whichMarkers(x, markers = markers, chroms = chroms, fromPos = fromPos, toPos = toPos)
   x$MARKERS[idx]
 }
@@ -68,21 +73,37 @@ removeMarkers = function(x, markers = NULL, chroms = NULL, fromPos = NULL, toPos
 #' @export
 whichMarkers = function(x, markers = NULL, chroms = NULL, fromPos = NULL, toPos = NULL) {
 
+  if(is.pedList(x)) {
+    idxList = lapply(x, function(comp)
+      whichMarkers(comp, markers = markers, chroms = chroms, fromPos = fromPos, toPos = toPos))
+    if(length(x) > 1 && !all(vapply(idxList[-1], identical, y = idxList[[1]], logical(1))))
+      stop2("The output of `whichMarkers()` differs between components")
+    return(idxList[[1]])
+  }
+
   # Early returns if nothing to do
   if(is.null(markers) && is.null(chroms) && is.null(fromPos) && is.null(toPos))
     return(integer(0))
-  if(!hasMarkers(x))
+
+  nMark = nMarkers(x)
+  if(nMark == 0)
     return(integer(0))
 
   stopifnotSimpleVector(markers, "markers")
 
-  if (is.numeric(markers)) {
+  if (is.logical(markers)) {
+    if(length(markers) != nMark)
+      stop2(sprintf("`markers` is a logical of length %d, which differs from the number of attached markers (%d)",
+                    length(markers), nMark))
+    idx = which(markers)
+  }
+  else if (is.numeric(markers)) {
     idx = as.integer(markers)
 
     if(any(markers != idx))
       stop2("Marker index must be integer: ", markers[markers != idx])
 
-    outside = idx < 1 | idx > nMarkers(x)
+    outside = idx < 1 | idx > nMark
     if(any(outside))
       stop2("Marker index out of range: ", markers[outside])
 
@@ -95,7 +116,7 @@ whichMarkers = function(x, markers = NULL, chroms = NULL, fromPos = NULL, toPos 
       stop2("Unknown marker name: ", markers[is.na(idx)])
   }
   else if(is.null(markers))
-    idx = seq_len(nMarkers(x))
+    idx = seq_len(nMark)
   else
     stop2("Argument `markers` must be a numeric, a character, or NULL")
 
