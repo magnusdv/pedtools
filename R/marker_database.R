4d218f62070f2c00a63f3aa7bde539b6d98ae04c
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2020-03-05 22:10:58 +0100

    New function `readFrequencyDatabase()`
    * Reorganise code and docs about frequency databases

--- R/marker_database.R
+++ R/marker_database.R
@@ -1,33 +1,55 @@
 #' Allele frequency database
 #'
-#' Extract or set marker allele frequencies using the 'allelic ladder' format
+#' Functions for reading, setting and extracting allele frequency databases, in
+#' either "list" format or "allelic ladder" format.
 #'
-#' These functions are essentially special cases of [getLocusAttributes()] and
-#' [setLocusAttributes()].
+#' A frequency database in "list" format is a list of numeric vectors; each
+#' vector named with the allele labels, and the list itself named with the
+#' marker names.
 #'
-#' If `database` is a data.frame, it must have the allele labels as row names
-#' and marker names as column names.
+#' Text files containing frequencies in "list" format should look as follows,
+#' where "marker1" and "marker2" are marker names, and "a1","a2",... are allele
+#' labels (which may be characters or numeric, but will always be converted to
+#' characters):
+#' \preformatted{
+#' marker1
+#' a1 0.2
+#' a2 0.5
+#' a3 0.3
 #'
-#' If `database` is a file path, it is read with the command
-#' `read.table(database, header = T, row.names = 1, as.is = T, check.names = F, ...)`
+#' marker2
+#' a1 0.9
+#' a2 0.1
+#' }
 #'
-#' @param x A `ped` object, or a list of such
-#' @param markers A character vector (with marker names) or a numeric vector
-#'   (with marker indices)
-#' @param database Either a data.frame or a character file path. See details.
-#' @param ... Optional arguments passed on to [read.table()]
+#' A database in "allelic ladder" format is rectangular, i.e., a numeric matrix
+#' (or data frame), with allele labels as row names and markers as column
+#' names. `NA` entries correspond to unobserved alleles.
+#'
+#' @param x	A ped object, or a list of such.
+#' @param markers	A character vector (with marker names) or a numeric vector
+#'   (with marker indices).
+#' @param database Either a list or matrix/data frame with allele frequencies,
+#'   or a file path (to be passed on to `readFrequencyDatabase()`).
+#' @param format Either "list" or "ladder".
+#' @param filename The path to a text file containing allele frequencies either
+#'   in "list" or "allelic ladder" format.
+#' @param ... Optional arguments passed on to [read.table()].
 #' @return
 #'
-#' * `getFrequencyDatabase` : a data.frame with allele labels as row names, and
-#' marker labels as column names
+#' * `getFrequencyDatabase`: either a list (if `format = "list"`) or a data
+#' frame (if `format = "ladder"`)
 #'
-#' * `getFrequencyDatabase`: a modified version of `x`
+#' * `readFrequencyDatabase`: a list (also if `format = "ladder"`) of named
+#' numeric vectors
+#'
+#' * `setFrequencyDatabase`: a modified version of `x`
 #'
 #' @seealso  [setLocusAttributes()], [setMarkers()], [setAlleles()]
 #'
 #' @examples
-#' loc1 = list(name = "m1", alleles = 3:4, afreq = c(.1, .9))
-#' loc2 = list(name = "m2", alleles = c("1", "10.2", "3"))
+#' loc1 = list(name = "m1", afreq = c(a = .1, b = .9))
+#' loc2 = list(name = "m2", afreq = c("1" = .2, "10.2" = .3, "3" = .5))
 #' x = setMarkers(singleton(1), locus = list(loc1, loc2))
 #' db = getFrequencyDatabase(x)
 #' db
@@ -37,7 +59,7 @@
 #'
 #' # The database can also be read directly from file
 #' tmp = tempfile()
-#' write.table(db, tmp, row.names = TRUE, col.names = TRUE)
+#' write("m1\na 0.1\nb 0.9\n\nm2\n1 0.2\n3 0.5\n10.2 0.3", tmp)
 #'
 #' z = setFrequencyDatabase(x, database = tmp)
 #' stopifnot(all.equal(x, z))
@@ -48,14 +70,16 @@ NULL
 
 #' @rdname freqDatabase
 #' @export
-getFrequencyDatabase = function(x, markers = NULL) {
+getFrequencyDatabase = function(x, markers = NULL, format = c("list", "ladder")) {
 
   # If list of pedigrees, use the first component
   if(is.pedList(x))
-    return(getFrequencyDatabase(x[[1]], markers = markers))
+    return(getFrequencyDatabase(x[[1]], markers = markers, format = format))
 
   if(!is.ped(x))
     stop2("Input must be a `ped` object or a list of such")
+
+  format = match.arg(format)
 
   attrs = getLocusAttributes(x, markers, attribs = c("name", "alleles", "afreq"))
   mnames = lapply(attrs, '[[', "name")
@@ -64,24 +88,27 @@ getFrequencyDatabase = function(x, markers = NULL) {
   als = lapply(attrs, '[[', "alleles")
   afr = lapply(attrs, '[[', "afreq")
 
-  allAlleles = unique.default(unlist(als))
+  if(format == "list") {
+    res = lapply(seq_along(attrs), function(i) setNames(afr[[i]], als[[i]]))
+    names(res) = mnames
+  }
+  if(format == "ladder") {
+    allAlleles = unique.default(unlist(als))
 
-  # Sort numerically if possible
-  nums = !anyNA(suppressWarnings(is.numeric(allAlleles)))
-  if(nums)
-    allAlleles = allAlleles[order(as.numeric(allAlleles))]
-  else
-    allAlleles = allAlleles[order(allAlleles)]
+    # Sort numerically if possible
+    if(!anyNA(suppressWarnings(is.numeric(allAlleles))))
+      allAlleles = allAlleles[order(as.numeric(allAlleles))]
+    else
+      allAlleles = allAlleles[order(allAlleles)]
 
-  # Create output matrix
-  res = matrix(NA, nrow = length(allAlleles), ncol = length(attrs),
-               dimnames = list(allAlleles, mnames))
+    # Create output matrix
+    res = matrix(NA, nrow = length(allAlleles), ncol = length(attrs),
+                 dimnames = list(allAlleles, mnames))
 
-  # Fill inn correct frequencies for each marker
-  for(i in seq_along(attrs)) {
-    als.i = als[[i]]
-    afr.i = afr[[i]]
-    res[als.i, i] = afr.i
+    # Fill inn correct frequencies for each marker
+    for(i in seq_along(attrs)) {
+      res[als[[i]], i] = afr[[i]]
+    }
   }
 
   res
@@ -89,59 +116,95 @@ getFrequencyDatabase = function(x, markers = NULL) {
 
 #' @rdname freqDatabase
 #' @export
-setFrequencyDatabase = function(x, database, ...) {
+setFrequencyDatabase = function(x, database, format = c("list", "ladder"), ...) {
 
-  # Read the table if file path
+  format = match.arg(format)
+
+  # If file path, read database
   if(is.character(database) && length(database) == 1) {
-    database = read.table(database, header = T, row.names = 1,
-                          as.is = T, check.names = F, ...)
+    database = readFrequencyDatabase(database, format = format, ...)
+    format = "list" # output is always in list format
   }
 
-  # Format 1: Allelic ladder
-  if(is.data.frame(database) || is.matrix(database))
-    loci = freqDb2attribList(database, format = "allelicLadder")
-  else if (is.list(database))
-    loci = freqDb2attribList(database, format = "list")
-
-  setLocusAttributes(x, locusAttributes = loci, matchNames = T, erase = F)
+  loci = freqDb2attribList(database, format = format)
+  setLocusAttributes(x, locusAttributes = loci, matchNames = TRUE, erase = FALSE)
 }
 
 
 # Conversion frequency database to list of locus attributes
-freqDb2attribList = function(database, format = c("list", "allelicLadder")) {
+freqDb2attribList = function(database, format = c("list", "ladder")) {
 
-  attrList = switch(match.arg(format),
-    # Format 1: List of named vectors
-    list = {
-      lapply(names(database), function(nm) {
-        v = database[[nm]]
-        als = names(v)
-        if(is.null(als))
-          stop2("When `database` is a list, then each entry must:\n",
-                " * be a numeric vector of allele frequencies\n",
-                " * be named with allelic labels")
-
-        list(name = nm, alleles = als, afreq = unname(v))
-      })
-    },
-    # Format 2: "Allelic ladder" as dataframe or matrix
-    allelicLadder = {
-      als = rownames(database)
-      nms = colnames(database)
-      if(dup <- anyDuplicated(nms))
-        stop2("Duplicated marker name in database file: ", nms[dup])
-
-      lapply(seq_along(nms), function(i) {
-
-        # Column of frequencies
-        frqs = database[, i]
-
-        # Index of rows with non-missing entries
-        idx = !is.na(frqs)
-
-        list(name = nms[i], alleles = als[idx], afreq = frqs[idx])
-      })
+  format = match.arg(format)
+  if(format == "list") {
+    attrList = lapply(names(database), function(nm) {
+      v = database[[nm]]
+      als = names(v)
+      if(is.null(als) || !is.numeric(v)) {
+        stop2("When `database` is a list, then each entry must:\n",
+              " * be a numeric vector of allele frequencies\n",
+              " * be named with allelic labels")
+      }
+      list(name = nm, alleles = als, afreq = unname(v))
     })
+  }
+  else if(format == "ladder") {
+    als = rownames(database)
+    nms = colnames(database)
+    if(dup <- anyDuplicated(nms))
+      stop2("Duplicated marker name in database file: ", nms[dup])
+
+    attrList = lapply(seq_along(nms), function(i) {
+      frqs = database[, i]
+      idx = !is.na(frqs)  # Index of non-missing entries
+      list(name = nms[i], alleles = als[idx], afreq = frqs[idx])
+    })
+  }
 
   attrList
+}
+
+
+#' @rdname freqDatabase
+#' @export
+readFrequencyDatabase = function(filename, format = c("list", "ladder"), ...) {
+
+  format = match.arg(format)
+
+  if(format == "list") {
+    # Read as table with two columns
+    raw = read.table(filename, colClasses = c("character", "numeric"),
+                     header = FALSE, fill = TRUE, blank.lines.skip = FALSE, ...)
+    raw = raw[raw$V1 != "", 1:2, drop = FALSE]
+
+    # First/last lines numbers for each marker
+    newMarker = which(is.na(raw$V2))
+    stops = c(newMarker[-1] -  1, nrow(raw))
+    nms = raw[newMarker, 1]
+
+    # Convert to list of named frequency vectors
+    res = lapply(seq_along(nms), function(i) {
+      m = raw[newMarker[i]:stops[i], , drop = FALSE]
+      als = m[-1, 1]
+      afr = m[-1, 2]
+      setNames(afr, als)
+    })
+  }
+  else if(format == "ladder") {
+
+    # Read entire data frame
+    database = read.table(filename, header = TRUE, row.names = 1,
+                          as.is = TRUE, check.names = F, ...)
+    als = rownames(database)
+    nms = colnames(database)
+
+    res = lapply(seq_along(nms), function(i) {
+      frqs = database[, i] # Column of frequencies
+      idx = !is.na(frqs)   # Index of rows with non-missing entries
+      setNames(frqs[idx], als[idx])
+      })
+  }
+
+  # Add marker names
+  names(res) = nms
+  res
 }
