b6aafc9b17bf6405b82a1465481538a1ceb9301e
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2020-07-26 11:19:57 +0200

    Revise frames & groupings in `pedPlotList`

--- R/ped_plot.R
+++ R/ped_plot.R
@@ -373,21 +373,19 @@ plot.pedList = function(x, ...) {
 #' Note that for tweaking dev.height and dev.width the function [dev.size()] is
 #' useful to determine the size of the active device.
 #'
-#' @param plot.arg.list A list of lists. Each element of `plot.arg.list` is a
-#'   list, where the first element is the [ped()] object to be plotted, and the
-#'   remaining elements are passed on to `plot.ped`. These elements must be
-#'   correctly named. See examples below.
+#' @param plots A list of lists. Each element of `plots` is a list, where the
+#'   first element is a pedigree, and the remaining elements are passed on to
+#'   `plot.ped`. These elements must be correctly named. See examples below.
 #' @param widths A numeric vector of relative widths of the subplots. Recycled
-#'   to `length(plot.arg.list)` if necessary, before passed on to [layout()].
-#'   Note that the vector does not need to sum to 1.
-#' @param frames Either a single logical (FALSE = no frames; TRUE = automatic
-#'   framing) or a list of numeric vectors: Each vector must consist of
-#'   consecutive integers, indicating subplots to be framed together. By default
-#'   the framing follows the list structure of `plot.arg.list`.
-#' @param frametitles A character vector of titles for each frame. If this is
-#'   non-NULL, titles for individuals subplots are ignored.
+#'   to `length(plots)` if necessary, before passed on to [layout()]. Note that
+#'   the vector does not need to sum to 1.
+#' @param groups A list of vectors, each consisting of consecutive integers, indicating subplots to be grouped. By default
+#'   the grouping follows the list structure of `plots`.
+#' @param titles A character vector of titles for each group. Overrides titles given in individuals subplots.
+#' @param frames A logical indicating if groups should be framed.
 #' @param fmar A single number in the interval \eqn{[0,0.5)} controlling the
 #'   position of the frames.
+#' @param frametitles Deprecated; use `titles` instead.
 #' @param newdev A logical, indicating if a new plot window should be opened.
 #' @param dev.height,dev.width The dimensions of the new plot window. If these
 #'   are NA suitable values are guessed from the pedigree sizes.
@@ -398,39 +396,41 @@ plot.pedList = function(x, ...) {
 #' @seealso [plot.ped()]
 #'
 #' @examples
-#' # Simplest use: Just give a list of ped objects.
-#' # To guess suitable plot window dimensions, use 'newdev = TRUE'
+#' ##################
+#' # Basic examples #
+#' ##################
+#'
+#' # Simples use: Just give a list of ped objects.
 #' peds = list(nuclearPed(3), cousinPed(2), singleton(12), halfSibPed())
 #' plotPedList(peds, newdev = TRUE)
 #'
 #' # Modify the relative widths (which are not guessed)
-#' widths = c(2, 3, 1, 2)
-#' plotPedList(peds, widths = widths)
+#' w = c(2, 3, 1, 2)
+#' plotPedList(peds, widths = w)
 #'
 #' # In most cases the guessed dimensions are ok but not perfect.
 #' # Resize plot window manually and re-plot with `newdev = FALSE` (default)
-#' # plotPedList(peds, widths = widths)
+#' # plotPedList(peds, widths = w)
 #'
 #' ## Remove frames
-#' plotPedList(peds, widths = widths, frames = FALSE)
+#' plotPedList(peds, widths = w, frames = FALSE)
 #'
-#' # Non-default frames
-#' frames = list(1, 2:3)
-#' plotPedList(peds, widths = widths, frames = frames,
-#'             frametitles = c('First', 'Second'))
+#' # Non-default grouping
+#' plotPedList(peds, widths = w, groups = list(1, 2:3), titles = 1:2)
 #'
-#' # Parameters common to all plots can be added in the main call:
-#' margins = c(2, 4, 2, 4)
-#' title = 'Same title'
-#' labs = ''
-#' symbolsize = 1.5
-#' plotPedList(peds, widths = widths, frames = frames, margins = margins,
-#'             title = title, labs = labs, symbolsize = symbolsize,
-#'             newdev = TRUE)
+#' # Parameters added in the main call are used in each sub-plot
+#' plotPedList(peds, widths = w, margins = c(2, 4, 2, 4), labs = leaves,
+#'             shaded = leaves, symbolsize = 1.3, col = list(red = 1))
 #'
-#' # COMPLEX EXAMPLE WITH MARKER DATA AND VARIOUS OPTIONS
-#' # For more control of individual plots, each plot and all its parameters
-#' # can be specified in its own list:
+#' ############################################################
+#' # Complex example with individual parameters for each plot #
+#' ############################################################
+#'
+#' # For more control of individual plots, each plot and all
+#' # its parameters can be specified in its own list.
+#'
+#' dev.off() # remove previous window
+#'
 #' x1 = nuclearPed(nch = 3)
 #' m1 = marker(x1, `3` = 1:2)
 #' marg1 = c(7, 4, 7, 4)
@@ -446,160 +446,178 @@ plot.pedList = function(x, ...) {
 #'
 #' x3 = singleton("Mr. X")
 #' marg3 = c(10, 0, 0, 0)
-#' plot3 = list(x3, margins = marg3, title = "Plot 3", symbolsize = 1, cex = 2)
+#' plot3 = list(x3, margins = marg3, title = "Plot 3",
+#'              symbolsize = 1, cex = 2)
 #'
 #' x4 = halfSibPed()
 #' shaded = 4:5
-#' col = c("black", "black", "black", "blue", "blue")
+#' col = list(red = founders(x4), blue = leaves(x4))
 #' marg4 = marg1
-#' plot4 = list(x4, margins = marg4, title = "Plot 4", shaded = shaded, col = col)
+#' plot4 = list(x4, margins = marg4, title = "Plot 4", cex = 1.3,
+#'              shaded = shaded, col = col)
 #'
 #' plotPedList(list(plot1, plot2, plot3, plot4), widths = c(2,3,1,2),
-#'             frames = list(1, 2:3, 4), newdev = TRUE)
+#'             groups = list(1, 2:3, 4), newdev = TRUE)
 #'
-#' # Different example:
+#'
+#' ################################
+#' # Example with large pedigrees #
+#' ################################
+#'
+#' # Important to set device dimensions here
+#'
 #' plotPedList(list(halfCousinPed(4), cousinPed(7)),
-#'             title = c('Many generations', 'Very many generations'),
-#'             dev.height = 9, dev.width = 9)
+#'             titles = c("Large", "Very large"),
+#'             dev.height = 8, dev.width = 5)
 #'
 #'
 #' @importFrom grDevices dev.new dev.size
 #' @importFrom graphics grconvertX grconvertY layout mtext rect par plot
 #' @export
-plotPedList = function(plot.arg.list, widths = NA, frames = TRUE,
-                       frametitles = names(plot.arg.list), fmar = NA,
-                       dev.height = NA, dev.width = NA,
-                       newdev = !is.na(dev.height) || !is.na(dev.width),
+plotPedList = function(plots, widths = NULL, groups = NULL, titles = NULL,
+                       frames = TRUE, fmar = NULL, frametitles = NULL,
+                       dev.height = NULL, dev.width = NULL,
+                       newdev = !is.null(dev.height) || !is.null(dev.width),
                        ...) {
 
-  plotlist.flattened = list()
-  if (deduceFrames <- isTRUE(frames)) {
-    frames = list()
+  if(!is.null(frametitles)) {
+    message("Argument `frametitles` is deprecated; use `titles` instead")
+    titles = frametitles
+  }
+
+  if(!(isTRUE(frames) || isFALSE(frames)))
+    stop2("`frames` must be either TRUE or FALSE; use `groups` to specify framing groups")
+
+  deduceGroups = is.null(groups)
+  if (deduceGroups) {
+    groups = list()
     k = 0
   }
-  for (p in plot.arg.list) {
+
+  # Flatten plot list
+  flatlist = list()
+
+  for (p in plots) {
     if (is.ped(p))
-      p = list(p)  # will now be included in next line
-    if (is.pedList(p)) {
-      plotlist.flattened = c(plotlist.flattened, lapply(p, list))
+      newpeds = list(list(p))
+    else if (is.pedList(p))
+      newpeds = lapply(newpeds, list)
+    else { # if list of ped with plot arguments
+      if (!is.ped(p[[1]]))
+        stop2("First element must be a `ped` object", p[[1]])
+      newpeds = list(p)
     }
-    else {
-        # if list of ped with plot arguments
-        if (!is.ped(p[[1]]))
-          stop2("First element must be a `ped` object", p[[1]])
-        p = list(p)
-        plotlist.flattened = append(plotlist.flattened, p)
-      }
-    if (deduceFrames) {
-      group = (k + 1):(k <- k + length(p))
-      frames = append(frames, list(group))
+
+    flatlist = c(flatlist, newpeds)
+    if (deduceGroups) {
+      groups = c(groups, list(k + seq_along(newpeds)))
+      k = k + length(newpeds)
     }
   }
 
-  N = length(plotlist.flattened)
-  if (identical(widths, NA))
-    widths = vapply(plotlist.flattened, function(p) ifelse(is.singleton(p[[1]]), 1, 2.5), 1)
-  else
-    widths = rep_len(widths, N)
-  maxGen = max(vapply(plotlist.flattened, function(arglist) .generations(arglist[[1]]), 1))
+  N = length(flatlist)
+  NG = length(groups)
 
-  if (hasframetitles <- !is.null(frametitles))
-    if(length(frametitles) != length(frames))
-      stop2(sprintf("Length of `frametitles` (%d) does not equal number of frames (%d)",
-            length(frametitles), length(frames)))
+  # Groups: check that each vector is consecutive integers, and no duplicates.
+  for (v in groups)
+    if (!is.numeric(v) || !isTRUE(all.equal.numeric(v, v[1]:v[length(v)])))
+      stop2("Each element of `groups` must consist of consecutive integers: ", v)
+  dup = anyDuplicated.default(unlist(groups))
+  if (dup > 0)
+    stop2("Plot occurring twice in `groups`: ", dup)
+
+  # Group titles
+  grouptitles = titles %||% names(plots)
+  if (!is.null(grouptitles) && length(grouptitles) != NG)
+    stop2(sprintf("Length of `titles` (%d) does not equal number of groups (%d)",
+                  length(grouptitles), NG))
+
+  # If no group titles, use inner titles if present
+  titles = grouptitles %||% sapply(flatlist, function(p) p$title %||% "")
+  hasTitles = any(titles != "")
+
+  # Relative plot widths
+  if (is.null(widths))
+    widths = vapply(flatlist, function(p) ifelse(is.singleton(p[[1]]), 1, 2.5), 1)
+  else {
+    if(!is.numeric(widths) && !length(widths) %in% c(1,N))
+      stop2("`widths` must be a numeric of length either 1 or the total number of objects")
+    widths = rep_len(widths, N)
+  }
+
+  maxGen = max(vapply(flatlist, function(arglist) .generations(arglist[[1]]), 1))
+
 
   extra.args = list(...)
-  if (!"title" %in% names(extra.args))
-    extra.args$title = ""
 
-  defaultmargins = if (N > 2)
-    c(0, 4, 0, 4) else c(0, 2, 0, 2)
+  defaultmargins = if (N > 2) c(0, 4, 0, 4) else c(0, 2, 0, 2)
 
-  plotlist.flattened = lapply(plotlist.flattened, function(arglist) {
+  # Prepare plot args for each ped
+  plotlist = lapply(flatlist, function(arglist) {
     names(arglist)[1] = "x"
-    g = .generations(arglist$x)
-    addMargin = 2 * (maxGen - g + 1)
-    if (!"margins" %in% c(names(arglist), names(extra.args)))
-      arglist$margins = defaultmargins + c(addMargin, 0, addMargin, 0)
 
-    # additional arguments given in (...)
+    # Additional arguments given in (...)
     for (parname in setdiff(names(extra.args), names(arglist)))
       arglist[[parname]] = extra.args[[parname]]
+
+    # Remove inner title
+    arglist$title = NULL
+
+    # Margins
+    arglist$margins = arglist$margins %||% {
+      g = .generations(arglist$x)
+      addMar = 2 * (maxGen - g + 1)
+      defaultmargins + c(addMar, 0, addMar, 0)
+    }
+
     arglist
   })
 
-  # title: this must be treated specially (in outer margins)
-  titles = sapply(plotlist.flattened, "[[", "title")
-  plotlist.flattened = lapply(plotlist.flattened, function(arglist) {
-    arglist$title = ""
-    arglist
-  })
-  hastitles = hasframetitles || any(titles != "")
-
-  # frame list: check that each vector is consecutive integers, and no duplicates.
-  if (is.list(frames)) {
-    for (v in frames)
-      if (!identical(TRUE, all.equal(v, v[1]:v[length(v)])))
-        stop2("Each element of `frames` must consist of consecutive integers: ", v)
-    dup = anyDuplicated.default(unlist(frames))
-    if (dup > 0)
-      stop2("Plot occurring twice in `frames` list: ", dup)
-  }
-
-
-  # create layout of plot regions and plot!
+  # Layout of plot regions
   if (newdev) {
-    if (is.na(dev.height))
-      dev.height = max(3, 1 * maxGen) + 0.3 * as.numeric(hastitles)
-    if (is.na(dev.width))
-      dev.width = 3 * N
+    dev.height = dev.height %||% max(3, 1 * maxGen) + 0.3 * as.numeric(hasTitles)
+    dev.width = dev.width %||% 3 * N
     dev.new(height = dev.height, width = dev.width, noRStudioGD = TRUE)
   }
 
-  new.oma = if (hastitles) c(0, 0, 3, 0) else c(0, 0, 0, 0)
+  new.oma = if (hasTitles) c(0, 0, 3, 0) else c(0, 0, 0, 0)
   opar = par(oma = new.oma, xpd = NA)
   on.exit(par(opar))
 
+  # Plot!
   layout(rbind(1:N), widths = widths)
-  for (arglist in plotlist.flattened)
+  for (arglist in plotlist)
     do.call(plot, arglist)
 
-  # leftmost coordinate of each plot region (converted to value in [0,1]).
+  # Leftmost coordinate of each plot region (converted to value in [0,1]).
   ratios = c(0, cumsum(widths)/sum(widths))
 
-  # add frames
-  if (is.list(frames)) {
-    midpoints = numeric()
-    fstart_index = sapply(frames, function(v) v[1])
-    fstop_index = sapply(frames, function(v) v[length(v)])
-    ratio_start = ratios[fstart_index]
-    ratio_stop = ratios[fstop_index + 1]  # fordi 0 foerst
-    midpoints = (ratio_start + ratio_stop)/2
+  # Group coordinates
+  grStartIdx = sapply(groups, function(v) v[1])
+  grStopIdx = sapply(groups, function(v) v[length(v)])
+  grStart = ratios[grStartIdx]
+  grStop = ratios[grStopIdx + 1]  # since 0 first
 
-    # margin (fmar): if NA, set to 5% of vertical height, but at most 0.25 inches.
-    if (is.na(fmar))
-      fmar = min(0.05, 0.25/dev.size()[2])
+  # Draw frames
+  if(frames) {
+    # Default margin: 5% of vertical height, but at most 0.25 inches.
+    fmar = fmar %||% min(0.05, 0.25/dev.size()[2])
+
     margPix = grconvertY(0, from = "ndc", to = "device") * fmar
     margXnorm = grconvertX(margPix, from = "device", to = "ndc")
-    frame_start = grconvertX(ratio_start + margXnorm, from = "ndc")
-    frame_stop = grconvertX(ratio_stop - margXnorm, from = "ndc")
+    frame_start = grconvertX(grStart + margXnorm, from = "ndc")
+    frame_stop = grconvertX(grStop - margXnorm, from = "ndc")
     rect(xleft = frame_start,
          ybottom = grconvertY(1 - fmar, from = "ndc"),
          xright = frame_stop,
          ytop = grconvertY(fmar, from = "ndc"), xpd = NA)
   }
 
-  cex.title =
-    if ("cex.main" %in% names(extra.args)) extra.args$cex.main
-    else NA
-
-  if (hasframetitles) {
-    for (i in 1:length(frames))
-      mtext(frametitles[i], outer = TRUE, at = midpoints[i], cex = cex.title)
-  }
-  else if (hastitles) {
-    for (i in 1:N)
-      mtext(titles[i], outer = TRUE, at = (ratios[i] + ratios[i + 1])/2, cex = cex.title)
+  # Add titles
+  if(hasTitles) {
+    midpoints = if(!is.null(grouptitles)) (grStart + grStop)/2 else ratios[1:N] + diff(ratios)/2
+    cex.title = extra.args$cex.main %||% NA
+    mtext(titles, outer = TRUE, at = midpoints, cex = cex.title)
   }
 }
 
