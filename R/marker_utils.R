6678313f12a2d8dc9e2e5d4a70bf9ef0c10793ce
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2022-04-20 22:12:23 +0200

    New function `addAllele()` for extending the allele set of a marker

--- R/marker_utils.R
+++ R/marker_utils.R
@@ -84,3 +84,58 @@ checkConsistency = function(x, mlist) {
 }
 
 
+#' Add allele
+#'
+#' Extends the allele set of a marker attached to a pedigree, by adding a single
+#' allele.
+#'
+#' @param x A `ped` object or a list of such.
+#' @param marker The name or index of a marker attached to `x`.
+#' @param allele The name of the new allele.
+#' @param freq The frequency of the new allele, by default 0.001.
+#' @param adjust Either "previous" or "all", indicating how the frequencies
+#'   should be adjusted so that they sum to 1. If "previous" (default), the
+#'   frequencies of the original alleles are multiplied with `1 - freq`. If
+#'   "all", scaling is performed after adding the new allele, i.e., dividing all
+#'   frequencies by `1 + freq`.
+#'
+#' @return A copy of `x` with modified marker attributes.
+#'
+#' @examples
+#' x = nuclearPed() |>
+#'   addMarker(geno = c(NA, NA, "b/c"), afreq = c(b = 0.5, c = 0.5))
+#'
+#' y = addAllele(x, marker = 1, allele = "a")
+#' afreq(y, 1)
+#'
+#' z = addAllele(y, marker = 1, allele = "d", freq = 0.1, adjust = "all")
+#' afreq(z, 1)
+#'
+#' @export
+addAllele = function(x, marker, allele, freq = 0.001, adjust = c("previous", "all")) {
+
+  if(missing(marker) || length(marker) == 0)
+    stop2("Argument `marker` cannot be empty")
+  if(length(marker) > 1)
+    stop2("Allele modifications must be done for a single marker at a time")
+  if(missing(allele) || length(allele) == 0)
+    stop2("Argument `allele` cannot be empty")
+  if(length(allele) > 1)
+    stop2("Please add one allele at a time")
+
+  old = afreq(x, marker)
+  if(allele %in% names(old))
+    stop2("This allele is already present")
+
+  names(freq) = allele
+  switch(match.arg(adjust),
+    all = {
+      newfr = c(old, freq)/(1 + freq)
+    },
+    previous = {
+      newfr = c(old * (1 - freq), freq)
+    }
+  )
+
+  setAfreq(x, marker, afreq = newfr, strict = FALSE)
+}
