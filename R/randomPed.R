168242c3a26a10da3f868593b21f973b838832c7
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2023-01-05 16:31:51 +0100

    `randomPed()` gains argument `maxGenerationGap`

--- R/randomPed.R
+++ R/randomPed.R
@@ -11,6 +11,9 @@
 #'   least 3.
 #' @param f A positive integer: the number of founders. Must be at least 2
 #'   unless selfing is allowed.
+#' @param maxGenerationGap An integer; the maximum distance in direct
+#'   descendence allowed in a mating. for example, the default value of 1 allows
+#'   parent-child mating, but not grandparent-grandchild.
 #' @param selfing A logical indicating if selfing is allowed. Default: FALSE.
 #' @param seed An integer seed for the random number generator (optional).
 #' @param g,founders Deprecated arguments.
@@ -22,7 +25,7 @@
 #' randomPed(8, f = 3, seed = 11, selfing = TRUE)
 #'
 #' @export
-randomPed = function(n, f = 2, selfing = FALSE, seed = NULL, g = NULL, founders = NULL) {
+randomPed = function(n, f = 2, maxGenerationGap = 1, selfing = FALSE, seed = NULL, g = NULL, founders = NULL) {
   # TODO: Remove
   if(!is.null(g)) {
     message("Switching to the old version, using deprecated argument `g`.")
@@ -63,8 +66,12 @@ randomPed = function(n, f = 2, selfing = FALSE, seed = NULL, g = NULL, founders 
   # Complete sex vector
   sex = c(sexFou, sexNonfou)
 
-  # List of components. Initially f singletons
+  # Component index.
+  # Initially 1,2,...f (singletons) and unknown (0) for the rest
   compvec = c(seq_len(f), rep(0L, n-f))
+
+  # Generation gap matrix
+  gengap = matrix(NA_integer_, nrow = n, ncol = n)
 
   for(k in seq(f + 1, to = n)) {
 
@@ -72,8 +79,11 @@ randomPed = function(n, f = 2, selfing = FALSE, seed = NULL, g = NULL, founders 
     ncomp = max(compvec)
     compSizes = tabulate(compvec)
 
-    # Surplus pairings
-    surplus = (n-k+1) - (ncomp-1)
+    # Minimum number of pairings required to make output connected
+    requ = ncomp-1
+
+    # Surplus pairings (i.e., leeway in addition to the required ones)
+    surplus = (n-k+1) - requ
 
     # Weights for choosing the first parent
     # The fewer surplus pairings, the more weight on small components.
@@ -86,14 +96,28 @@ randomPed = function(n, f = 2, selfing = FALSE, seed = NULL, g = NULL, founders 
     par1 = sample.int(k-1, 1, prob = w1)
     comp1 = compvec[par1]
 
-    # Second parent: Any of opposite sex.
+    # Candidates for the other parent: Any of opposite sex
     isCand = sex[sq] != sex[par1]
+
+    # If selfing, include self as candidate
     if(selfing)
       isCand[par1] = TRUE
 
     # If no surplus, partner cannot be in same component
     if(surplus == 0)
       isCand[compvec[sq] == comp1] = FALSE
+
+    # Apply generation gap limit if given
+    if(maxGenerationGap < Inf) {
+      gaps = pmax(gengap[sq, par1], gengap[par1, sq], na.rm = TRUE)
+      toofar = !is.na(gaps) & gaps > maxGenerationGap
+      isCand[toofar] = FALSE
+    }
+
+    if(!any(isCand)) {
+      p = ped(id[sq], fid[sq], mid[sq], sex[sq])
+      plot(p, hatched = par1)
+    }
 
     # The fewer surplus, the more probable to go to another comp
     par2 = safe_sample(sq[isCand], 1)
@@ -123,6 +147,13 @@ randomPed = function(n, f = 2, selfing = FALSE, seed = NULL, g = NULL, founders 
       b = compvec > max(comp1, comp2)
       compvec[b] = compvec[b] - 1
     }
+
+    # Update generation gap matrix
+    gengap[sq, k] = 1L + pmax(gengap[sq, par1], gengap[sq, par2], na.rm = TRUE)
+    if(is.na(gengap[par1, k]))
+      gengap[par1, k] = 1L
+    if(is.na(gengap[par2, k]))
+      gengap[par2, k] = 1L
   }
 
   newPed(as.character(id), as.integer(fid), as.integer(mid), sex, FAMID = "")
