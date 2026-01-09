0b82cae1c8ad2d695a3ffb250667e114563b35bb
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2026-01-09 08:53:11 +0100

    New fs `expectedHomozygosity()` and `expectedHeterozygosity()`

--- R/marker_stats.R
+++ R/marker_stats.R
@@ -29,3 +29,39 @@ countHomozygous = function(x, ids = NULL) {
   names(res) = ids
   res
 }
+
+
+#' Expected homozygosity and heterozygosity
+#'
+#' Computes the expected homozygosity and heterozygosity for a marker from its
+#' allele frequencies. The homozygosity is \eqn{\sum_i p_i^2} and the
+#' heterozygosity is one minus this.
+#'
+#' @param p A numeric vector of allele frequencies, or a `marker` object (from
+#'   which the frequencies are extracted with [afreq()]).
+#'
+#' @returns A numeric value giving the expected homozygosity or heterozygosity.
+#'
+#' @examples
+#' p = c(0.2, 0.5, 0.3)
+#' expectedHomozygosity(p)
+#' expectedHeterozygosity(p)
+#'
+#' @export
+expectedHomozygosity = function(p) {
+  if(is.marker(p))
+    p = afreq(p)
+  else if(!is.numeric(p) || any(p < 0) || round(sum(p), 3) != 1)
+    stop2("`p` is neither a `marker` object nor a valid frequency vector: ", p)
+  sum(p^2)
+}
+
+#' @rdname expectedHomozygosity
+#' @export
+expectedHeterozygosity = function(p) {
+  if(is.marker(p))
+    p = afreq(p)
+  else if(!is.numeric(p) || any(p < 0) || round(sum(p), 3) != 1)
+    stop2("`p` is neither a `marker` object nor a valid frequency vector: ", p)
+  1 - sum(p^2)
+}
