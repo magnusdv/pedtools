cee75d4066ac9957d44af74a4f82d0d7e39081b0
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2026-01-09 10:30:14 +0100

    New function `sameGenotype()`

--- R/marker_stats.R
+++ R/marker_stats.R
@@ -28,6 +28,44 @@ countHomozygous = function(x, ids = NULL) {
   res = .rowSums(homoz, dm[1], dm[2]/2, na.rm = TRUE)
   names(res) = ids
   res
+#' Find markers for which two individuals have the same genotype
+#'
+#' Identifies markers for which two individuals have the same (non-missing)
+#' genotype. The comparison is done after sorting the genotypes internally.
+#'
+#' @param x A `ped` object or a list of such. An error is raised if `x` has no
+#'   marker data.
+#' @param ids A vector of two individual ID labels.
+#' @param count A logical. If TRUE, return the number of markers with shared
+#'   genotype.
+#'
+#' @returns A logical vector with one entry per marker (`NA` if either genotype
+#'   is missing). If `count = TRUE`, the number of `TRUE` entries.
+#'
+#' @seealso [isHomozygous()], [sortGenotypes()]
+#'
+#' @examples
+#' x = nuclearPed() |>
+#'   addMarker(name = "m1", geno = c(NA, "1/1", "1/2")) |>
+#'   addMarker(name = "m2", geno = c(NA, "1/2", "2/1"))
+#'
+#' sameGenotype(x, 2:3)
+#' sameGenotype(x, 2:3, count = TRUE)
+#'
+#' @export
+sameGenotype = function(x, ids = typedMembers(x), count = FALSE) {
+  if(!hasMarkers(x)) stop2("Input pedigree has no marker data")
+  if(length(ids) != 2) stop2("Argument `ids` must be a vector of length 2")
+
+  x = sortGenotypes(x)
+
+  gmat = getGenotypes(x, ids)
+  same = gmat[1, ] == gmat[2, ]
+
+  if(count)
+    return(sum(same, na.rm = TRUE))
+
+  same
 }
 
 
